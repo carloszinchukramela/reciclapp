8d884b74422b24fa07f1e714f2ddbc4613859d96
unknown <carloszinchukramela@gmail.com>
2020-09-11 11:34:40 -0300

    Agregué panel de adm
    usuario: usuario
    contraseña: usuario

--- paginaPrincipal/admin.py
+++ paginaPrincipal/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from paginaPrincipal.models import TipoDesecho
+from paginaPrincipal.models import Oferente
+
 # Register your models here.
+
+admin.site.register(TipoDesecho)
+admin.site.register(Oferente)
